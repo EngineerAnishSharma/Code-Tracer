89a8bd6af53221c3e4a98f82a69c331305752671
Anish <engineeranish10000@gmail.com>
2025-03-07 10:26:28 +0530

    bubble sort

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from wrapper import stepbystep_wrapper
 
 @stepbystep_wrapper(time_between_steps=1)
 def sample_function():
-    x = 10
-    y = 20
-    z = x + y
-    print("Result:", z)
+    arr = [64, 34, 25, 12, 22, 11, 90]
+
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    print("Sorted array is:", arr)
 
 sample_function()
